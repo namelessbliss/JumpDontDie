10ba221097c883d29a6cb7e6cddccc8912b03b92
namelessbliss <anonimo2911.2012@gmail.com>
2018-03-02 17:23:50 -0500

    Cuarto commit comprobar colisiones

--- core/src/com/jumpdontdie/MainGameScreen.java
+++ core/src/com/jumpdontdie/MainGameScreen.java
@@ -3,8 +3,10 @@ package com.jumpdontdie;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.jumpdontdie.actors.ActorJugador;
+import com.jumpdontdie.actors.ActorPinchos;
 
 /**
  * Created by Administrador on 01/03/2018.
@@ -13,24 +15,36 @@ import com.jumpdontdie.actors.ActorJugador;
 public class MainGameScreen extends BaseScreen {
     public MainGameScreen(MyGdxGame game) {
         super(game);
-        texturaJugador = new Texture("player1.png;");
+        texturaJugador = new Texture("player1.png");
+        texturaPinchos = new Texture("pinchos.png");
+        regionPinchos = new TextureRegion(texturaPinchos,0,64,128,64);
     }
 
     private Stage stage;
-
+//Declarar actores
     private ActorJugador jugador;
 
-    private Texture texturaJugador;
-
+    private ActorPinchos pinchos;
+//Declarar texturas
+    private Texture texturaJugador,texturaPinchos;
+//Declarar region de textura
+    private TextureRegion regionPinchos;
 
     @Override
     public void show() {
         stage = new Stage();
-        jugador = new ActorJugador(texturaJugador);
-        stage.addActor(jugador);
+        //dibujar bordes de obejtos
+        stage.setDebugAll(true);
 
-        //definir posision jugador
+        jugador = new ActorJugador(texturaJugador);
+        pinchos = new ActorPinchos(regionPinchos);
+        stage.addActor(jugador);
+        stage.addActor(pinchos);
+
+
+        //definir posision de actores
         jugador.setPosition(20,100);
+        pinchos.setPosition(500,100);
     }
 
     @Override
@@ -43,11 +57,23 @@ public class MainGameScreen extends BaseScreen {
         Gdx.gl.glClearColor(0.4f, 0.5f, 0.8f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
+
+        comprobarColisiones();
+
         stage.draw();
+    }
+
+    private void comprobarColisiones(){
+        if (jugador.isAlive() &&
+                jugador.getX()+jugador.getWidth()>pinchos.getX()){
+            System.out.println("COLISION");
+            jugador.setAlive(false);
+        }
     }
 
     @Override
     public void dispose() {
         texturaJugador.dispose();
+        texturaPinchos.dispose();
     }
 }
--- core/src/com/jumpdontdie/actors/ActorPinchos.java
+++ core/src/com/jumpdontdie/actors/ActorPinchos.java
@@ -1,5 +1,6 @@
 package com.jumpdontdie.actors;
 
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 
@@ -12,6 +13,21 @@ public class ActorPinchos extends Actor {
     private TextureRegion pinchos;
 
     public ActorPinchos(TextureRegion pinchos){
+        this.pinchos = pinchos;
+        //establecer tamaño
+        setSize(pinchos.getRegionWidth(),pinchos.getRegionHeight());
 
+    }
+
+    @Override
+    public void act(float delta) {
+        //avanza hacia la izq 250 px por seg
+        //delta cuenta los miliseg que pasan
+        setX(getX() - 250 * delta);
+    }
+
+    @Override
+    public void draw(Batch batch, float parentAlpha) {
+        batch.draw(pinchos,getX(),getY());
     }
 }
