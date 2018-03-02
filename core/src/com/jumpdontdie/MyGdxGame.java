d24dc698e7cc2b01d3122568ea1f134758720776
namelessbliss <anonimo2911.2012@gmail.com>
2018-03-01 20:20:34 -0500

    Tercer commit stages y actores

--- core/src/com/jumpdontdie/MyGdxGame.java
+++ core/src/com/jumpdontdie/MyGdxGame.java
@@ -1,36 +1,12 @@
 package com.jumpdontdie;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
 
-public class MyGdxGame extends ApplicationAdapter {
+import com.badlogic.gdx.Game;
 
+public class MyGdxGame extends Game {
 
-	@Override
-	public void create() {
-		Procesador p = new Procesador();
-		Gdx.input.setInputProcessor(p);
-	}
-
-	@Override
-	public void dispose() {
-
-	}
-
-	@Override
-	public void render() {
-		//limpiar la los colores en pantalla pantalla
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		//Colorear pantalla
-		Gdx.gl.glClearColor(0.3f,0.3f,0.3f,1);
-
-		/*//Metodo de entrada
-		if (Gdx.input.justTouched()) {
-			System.out.println("Estas tocando la pantalla");
-
-		}*/
-
-
-	}
+    @Override
+    public void create() {
+        setScreen(new MainGameScreen(this));
+    }
 }
