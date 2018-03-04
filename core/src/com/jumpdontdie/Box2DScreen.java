248848074222ddb693a929b85b3497a4cd58f680
namelessbliss <anonimo2911.2012@gmail.com>
2018-03-04 17:08:40 -0500

    quinto commit dibujar triangulo con vectores

---
+++ core/src/com/jumpdontdie/Box2DScreen.java
@@ -0,0 +1,113 @@
+package com.jumpdontdie;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+
+/**
+ * Created by Administrador on 02/03/2018.
+ */
+
+public class Box2DScreen extends BaseScreen {
+    public Box2DScreen(MyGdxGame game) {
+        super(game);
+    }
+
+    private World world;
+
+    private Box2DDebugRenderer renderer;
+
+    private OrthographicCamera camera;
+
+    private Body player1Body, sueloBody, pinchoBody;
+
+    private Fixture player1Fixture, sueloFixture, pinchoFixture;
+
+    @Override
+    public void show() {
+        //declarar nuevo mundo
+        world = new World(new Vector2(0, -10),true);
+        renderer = new Box2DDebugRenderer();
+        //crear camara
+        //tamaño en metros
+        camera = new OrthographicCamera(7.11f ,4);
+        camera.translate(0,1);
+//crear bodies
+        player1Body = world.createBody(createPlayerBodyDef());
+        sueloBody = world.createBody(createSueloBodyDef());
+        pinchoBody = world.createBody(createPinchoBody(0.5f));
+
+        PolygonShape player1Shape = new PolygonShape();
+        player1Shape.setAsBox(0.5f,0.5f);
+        player1Fixture = player1Body.createFixture(player1Shape,1);
+        player1Shape.dispose();
+
+        PolygonShape sueloShape = new PolygonShape();
+        sueloShape.setAsBox(500,1);
+        sueloFixture = sueloBody.createFixture(sueloShape, 1);
+        sueloShape.dispose();
+
+        pinchoFixture = createPincoFixture(pinchoBody);
+    }
+
+    private BodyDef createPinchoBody(float x) {
+        BodyDef def = new BodyDef();
+        def.position.set(x,0.5f);
+        return def;
+    }
+
+    private BodyDef createSueloBodyDef() {
+        BodyDef def = new BodyDef();
+        def.position.set(0,-1);
+        return def;
+    }
+
+    private BodyDef createPlayerBodyDef() {
+        BodyDef def = new BodyDef();
+        def.position.set(0,10);
+        def.type = BodyDef.BodyType.DynamicBody;
+        return def;
+    }
+
+    private Fixture createPincoFixture(Body pinchoBody){
+        //hacer triangulo
+        Vector2[] vertices = new Vector2[3];
+        vertices[0] = new Vector2(-0.5f,-0.5f);
+        vertices[1] = new Vector2(0.5f,-0.5f);
+        vertices[2] = new Vector2(0,0.5f);
+        PolygonShape shape = new PolygonShape();
+        shape.set(vertices);
+        Fixture fix = pinchoBody.createFixture(shape,1);
+        shape.dispose();
+        return fix;
+    }
+
+    @Override
+    public void dispose() {
+        player1Body.destroyFixture(player1Fixture);
+        sueloBody.destroyFixture(sueloFixture);
+        pinchoBody.destroyFixture(pinchoFixture);
+        world.destroyBody(player1Body);
+        world.destroyBody(sueloBody);
+        world.destroyBody(pinchoBody);
+        world.dispose();
+        renderer.dispose();
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+//establecer mundo
+        world.step(delta, 6,2);
+        //dibujar mundo
+        camera.update();
+        renderer.render(world,camera.combined);
+    }
+}
--- core/src/com/jumpdontdie/MyGdxGame.java
+++ core/src/com/jumpdontdie/MyGdxGame.java
@@ -7,6 +7,6 @@ public class MyGdxGame extends Game {
 
     @Override
     public void create() {
-        setScreen(new MainGameScreen(this));
+        setScreen(new Box2DScreen(this));
     }
 }
